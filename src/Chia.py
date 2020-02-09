c0cc55b42293ac409bb58a9feb7004d0b2ebb52b
Ya Ting Candy Sun <ysun106@binghamton.edu>
2020-02-08 22:22:15 -0500

    Added Chia to screen

--- src/Chia.py
+++ src/Chia.py
@@ -25,65 +25,65 @@ class Chia(pygame.sprite.Sprite):
 
 
 
-     def default(self, w, h)
+     def default(self, w, h):
          self.image = pygame.transform.scale(pygame.image.load("assets/Chia_Standing.PNG").convert_alpha(),(w,h))
 
 
      def happy(self, w, h):
-         for i in range(3)
+         for i in range(3):
              self.image = pygame.transform.scale(pygame.image.load("assets/Chia_Happy.PNG").convert_alpha(),(w,h))
          self.image = pygame.transform.scale(pygame.image.load("assets/Chia_Standing.PNG").convert_alpha(),(w,h))
 
      def angry(self, w, h):
-         for i in range(3)
+         for i in range(3):
              self.image = pygame.transform.scale(pygame.image.load("assets/Chia_Angry.PNG").convert_alpha(),(w,h))
 
      def walk(self, w, h):
          '''
             Controls walking 
          '''
-         if self.rect.x >= h/2 and self.rect.x < h:
+         if self.rect.x >= (h/2)+100 and self.rect.x < h-100:
             self.direction = random.choice(["right","left"])
 
-            if self.direction = "left":
-               for i in random.choice([1,2,3,4,5])
+            if (self.direction == "left"):
+               for i in random.choice([1,2,3,4,5]):
                    self.index += 1
-                       if self.index > len(self.walking):
-                          self.index = 0
+                   if self.index > len(self.walking):
+                      self.index = 0
                    self.rect.x -= 1
                self.image = pygame.transform.scale(pygame.image.load("assets/Chia_Standing.PNG").convert_alpha(),(w,h))
                
 
-            if self.direction = "right"
+            if (self.direction == "right"):
                self.image = self.walking[self.index]
                self.image = pygame.transform.flip(self.walking, True, False)
-               for i in random.choice([1,2,3,4,5])
+               for i in random.choice([1,2,3,4,5]):
                    self.index += 1
-                       if self.index > len(self.walking):
-                          self.index = 0
+                   if self.index > len(self.walking):
+                      self.index = 0
                    self.rect.x += 1
                self.image = pygame.transform.flip(self.walking, True, False)
                self.image = pygame.transform.scale(pygame.image.load("assets/Chia_Standing.PNG").convert_alpha(),(w,h))
 
          #To prevent going out of range
-         elif self.rect.x < h/2
+         elif (self.rect.x < (h/2)+100):
             self.direction = "right"
-               for i in random.choice([1,2,3,4,5])
-                   self.index += 1
-                       if self.index > len(self.walking):
-                          self.index = 0
-                   self.rect.x += 1
+            for i in random.choice([1,2,3,4,5]):
+                self.index += 1
+                if self.index > len(self.walking):
+                   self.index = 0
+                self.rect.x += 1
             self.image = pygame.transform.flip(self.walking, True, False)
             self.image = pygame.transform.scale(pygame.image.load("assets/Chia_Standing.PNG").convert_alpha(),(w,h))
 
-         else self.rect.x > 2:
-            if self.direction = "left":
-               for i in random.choice([1,2,3,4,5])
-                   self.index += 1
-                       if self.index > len(self.walking):
-                          self.index = 0
-                   self.rect.x -= 1
-               self.image = pygame.transform.scale(pygame.image.load("assets/Chia_Standing.PNG").convert_alpha(),(w,h))
+         elif self.rect.x > h-100:
+            self.direction = "left"
+            for i in random.choice([1,2,3,4,5]):
+                self.index += 1
+                if self.index > len(self.walking):
+                   self.index = 0
+                self.rect.x -= 1
+            self.image = pygame.transform.scale(pygame.image.load("assets/Chia_Standing.PNG").convert_alpha(),(w,h))
          
                     
 
--- src/controller.py
+++ src/controller.py
@@ -4,7 +4,7 @@ import random
 import time
 from src import Button
 from src import Line
-#from src import Chia
+from src import Chia
 from src import Sian
 from src import Crow
 
@@ -36,7 +36,7 @@ class Controller:
         self.ground = Button.Button(0, 793, 163, 1700, "assets/GameScreen_Ground.PNG")
         self.crow = Crow.Crow(35, 200, 128, 163, "assets/Crow1.PNG", "assets/Crow2.PNG")
         self.theline = Line.Line(150, 600, 10, 10, "assets/Dot.PNG")
-        self.chia = Chia.Chia(163, 435, 221, 365, "assets/Chia_Standing.PNG")
+        self.chia = Chia.Chia(1400, 435, 221, 365, "assets/Chia_Standing.PNG")
         self.sian = Sian.Sian(50, 435, 219, 364, "assets/Sian_Empty.PNG")
         self.empty = True
         self.holding_object = False
